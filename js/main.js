eb1b1aa365853389931fe8269762106a3bdefad5
gomzyakov <alexander.gomzyakov@gmail.com>
2019-08-19 15:42:01 +0500

    Delete featured-projects block

--- js/main.js
+++ js/main.js
@@ -52,6 +52,5 @@ $(function () {
   sr.reveal('.background');
   sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
-  sr.reveal('.featured-projects', { viewFactor: 0.1 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
