137fc85aa7d87a5d0e9d51e87515d0a922b74fc4
gomzyakov <alexander.gomzyakov@gmail.com>
2023-02-07 20:54:59 +0500

    feat: move `good-boy` repository here

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const images = require('./build/images');
-const sync = require('./build/browsersync');
-
-[images, sync].forEach(task => {
-  task(gulp);
-});
-
-gulp.task('build', ['images', 'jekyll-build']);
