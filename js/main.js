f4a7eb87d2cceda2fe339238a8590a913ea5546a
gomzyakov <alexander.gomzyakov@gmail.com>
2023-02-23 14:36:29 +0500

    Remove `skills`

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ $(function () {
   });
 
   sr.reveal('.background');
-  sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
