e75a98dc23750f0a8a42c429262b11b0a3340b36
gomzyakov <alexander.gomzyakov@gmail.com>
2023-02-23 14:33:11 +0500

    Remove `top-button`

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 $(function () {
 
-  const introHeight = document.querySelector('.intro').offsetHeight;
-  const topButton = document.getElementById('top-button');
-  const $topButton = $('#top-button');
-
-  window.addEventListener(
-    'scroll',
-    function () {
-      if (window.scrollY > introHeight) {
-        $topButton.fadeIn();
-      } else {
-        $topButton.fadeOut();
-      }
-    },
-    false
-  );
-
-  topButton.addEventListener('click', function () {
-    $('html, body').animate({ scrollTop: 0 }, 500);
-  });
-
   const hand = document.querySelector('.emoji.wave-hand');
 
   function waveOnLoad() {
