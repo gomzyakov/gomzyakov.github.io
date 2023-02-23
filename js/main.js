dd13650192a057bcaa34c620993f9386dd939214
Alexander Gomzyakov <alexander.gomzyakov@gmail.com>
2023-02-23 19:37:05 +0500

    Common updates for personal website (#41)

--- js/main.js
+++ js/main.js
@@ -1,25 +1,5 @@
 $(function () {
 
-  const introHeight = document.querySelector('.intro').offsetHeight;
-  const topButton = document.getElementById('top-button');
-  const $topButton = $('#top-button');
-
-  window.addEventListener(
-    'scroll',
-    function () {
-      if (window.scrollY > introHeight) {
-        $topButton.fadeIn();
-      } else {
-        $topButton.fadeOut();
-      }
-    },
-    false
-  );
-
-  topButton.addEventListener('click', function () {
-    $('html, body').animate({ scrollTop: 0 }, 500);
-  });
-
   const hand = document.querySelector('.emoji.wave-hand');
 
   function waveOnLoad() {
@@ -50,7 +30,6 @@ $(function () {
   });
 
   sr.reveal('.background');
-  sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
